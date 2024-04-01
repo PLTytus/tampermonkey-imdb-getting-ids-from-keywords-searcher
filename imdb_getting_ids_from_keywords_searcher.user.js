5952b74699a07a89feeeab5d9ce77e6d35d4b9f0
PLTytus <rafal.tytus98@gmail.com>
2024-04-01 18:40:44 +0200

    moved to github

--- imdb_getting_ids_from_keywords_searcher.user.js
+++ imdb_getting_ids_from_keywords_searcher.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         IMDb - Getting IDs from keywords searcher
 // @namespace    http://gtaweb.eu/tampermonkey
-// @version      0.1
+// @version      0.2
 // @author       PLTytus
-// @downloadURL  https://bitbucket.org/PLTytus/imdb-getting-ids-from-keywords-searcher/raw/master/imdb_getting_ids_from_keywords_searcher.user.js
-// @updateURL    https://bitbucket.org/PLTytus/imdb-getting-ids-from-keywords-searcher/raw/master/imdb_getting_ids_from_keywords_searcher.meta.js
+// @downloadURL  https://github.com/PLTytus/tampermonkey-imdb-getting-ids-from-keywords-searche/raw/master/imdb_getting_ids_from_keywords_searcher.user.js
+// @updateURL    https://github.com/PLTytus/tampermonkey-imdb-getting-ids-from-keywords-searche/blob/master/imdb_getting_ids_from_keywords_searcher.meta.js
 // @match        https://www.imdb.com/search/keyword/?keywords=*
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.6.1.min.js
